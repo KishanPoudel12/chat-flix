a9e6a460094a1200a2476b0080f4ceec0b03af42
kishan <kishanpoudel563@gmail.com>
2026-01-02 15:26:49 +0530

    cors

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ from websocket.demo import html
 #routers import
 from routers import user_router, ws_router,message_router,room_router
 from auth import  auth_router
-
+from fastapi.middleware.cors import  CORSMiddleware
 app = FastAPI()
+
+
+
+origins = [
+    "http://localhost:5173",  # frontend URL
+    "http://127.0.0.1:5173",  # frontend dev server
+    "https://chat-flix-kishan.onrender.com",  # your hosted frontend
+    "*",  # temporary: allow all origins
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
