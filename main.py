7b1eae6a5eeef9b542f46b21ade553ef4d573f5c
kishan <kishanpoudel563@gmail.com>
2026-01-05 13:58:13 +0530

    cors error to solve

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 
 origins = [
-    "http://localhost:5173",  # frontend URL
+    "http://localhost:3000",  # frontend URL
     "http://127.0.0.1:5173",  # frontend dev server
     "https://chat-flix-kishan.onrender.com",  # your hosted frontend
     "*",  # temporary: allow all origins
