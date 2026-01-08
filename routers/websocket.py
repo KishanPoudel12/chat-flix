ba7c15e88b1b0034f0a80931fe0fe325477375dd
kishan <kishanpoudel563@gmail.com>
2026-01-08 20:26:59 +0530

    websocket response made okay

--- routers/websocket.py
+++ routers/websocket.py
@@ -88,7 +88,7 @@ async def join(room_id:int , websocket:WebSocket, db:Session=Depends(get_db)):
                 break
 
             elif data.get("type")=="chat" :
-                await manager.broadcast(room_id=room_id, message={"type":"chat", "message":f"{current_user.username}:{data["message"]}"})
+                await manager.broadcast(room_id=room_id, message={"type":"chat", "message":data["message"],"sender" :current_user.username  })
     except WebSocketDisconnect:
         manager.disconnect(room_id, user_id=current_user.id,db=db)
         await manager.broadcast(room_id,message={"type":"system", "message":f"User {current_user.id} Disconnected"} )
