f55ba6def19e542d28c8196394530cdfb7e8bc25
kishan <kishanpoudel563@gmail.com>
2026-01-09 19:09:47 +0530

    trailing slash remove

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ origins = [
     "http://127.0.0.1:5173",  # frontend dev server
     "https://chat-flix-kishan.onrender.com",  # your hosted frontend
     "https://chat-flix-d92mas0cu-kishanpoudel563-8257s-projects.vercel.app",
-    "https://chat-flix-gamma.vercel.app/"
+    "https://chat-flix-gamma.vercel.app"
 ]
 
 app.add_middleware(
